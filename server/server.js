949f2be4b41365ec3ddf2920f1daac92e733973b
Noah Burton <noahburton@Noahs-MacBook-Air.local>
2023-11-26 13:27:33 -0700

    server.js

---
+++ server/server.js
@@ -0,0 +1,23 @@
+// import nessesary modules
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const mongoose = require('mongoose');
+const cors = require('cors');
+//todo import schema
+
+const app = express();
+app.use(cors());
+
+mongoose.connect('mongodb://localhost:27017/vibe-check');
+mongoose.connection.once('open', () => {
+    console.log('Connected to database');
+});
+
+app.use('/graphql', graphqlHTTP({
+    //todo add schema
+    graphiql: true
+}));
+
+app.listen(4000, () => {
+    console.log('Now listening for requests on port 4000');
+});
