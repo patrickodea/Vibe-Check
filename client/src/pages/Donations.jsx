036bc7f212637885e4248f560ba7c7a2f4eac2d5
Dae <daestanii1997@gmail.com>
2023-12-04 18:27:44 -0700

    formatted logo and added payment link

--- client/src/pages/Donations.jsx
+++ client/src/pages/Donations.jsx
@@ -3,6 +3,7 @@ const Donations = () => {
     return (
         <div>
             <h3>Donations page</h3>
+            <a href="https://buy.stripe.com/test_bIYbLA4rc34ta9WcMM">Click here to donate!</a>
         </div>
     )
 }
